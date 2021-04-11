149539ce13465134cdc01dc1892809bfb04c7347
Nelson Tobon <nett9723@gmail.com>
2021-04-11 12:51:14 -0500

    Estudios

--- js/app.js
+++ js/app.js
@@ -9,19 +9,19 @@ var seccionesPagina = new fullpage('#fullpage', {
     scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
     css3: true, // Si usara CSS3 o javascript.
     easingcss3: 'ease-out', // Curva de velocidad del efecto.
-    loopBottom: true, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+    loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
     // ──────────────────────────────────────────────────
     //   :::::: Barra de navegación
     // ──────────────────────────────────────────────────
     navigation: true, // Muesta la barra de navegación.
     menu: '#menu', // Menu de navegación.
-    anchors: ['inicio', 'productos', 'contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
-    navigationTooltips: ['Inicio', 'Productos', 'Contacto'], // Tooltips que mostrara por cada boton.
+    anchors: ['inicio', 'informacion', 'educacion', 'experiencia','contacto'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    navigationTooltips: ['inicio', 'informacion', 'educacion', 'experiencia','contacto'], // Tooltips que mostrara por cada boton.
     showActiveTooltip: false, // Mostrar tooltip activa.
     // ──────────────────────────────────────────────────
     //   :::::: Secciones
     // ──────────────────────────────────────────────────
-    sectionsColor: ['#000', 'blue', '#000'], // Color de fondo de cada seccion.
+    sectionsColor: ['#000', 'blue', 'red','green','#000'], // Color de fondo de cada seccion.
     verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
     // ──────────────────────────────────────────────────
     //   :::::: Slides
